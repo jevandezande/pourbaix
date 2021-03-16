584f36edcd0142983a50782b45bc00c4b158ddaa
Jonathon Vandezande <jevandezande@gmail.com>
2021-03-15 22:29:14 -0400

    Adds types and updates formatting.

--- pourbaix.py
+++ pourbaix.py
@@ -1,50 +1,85 @@
 #!/usr/bin/env python3
+from __future__ import annotations
 
+from typing import Tuple
+
+import matplotlib
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+MPLFig = Tuple[matplotlib.figure.Figure, matplotlib.axes.Subplot]
 
 SLOPE = -0.059
 ROTATION = -8
 
 
-def water(plot=None, phases=True, reactions=True, x1=0, x2=14):
+def water(
+    plot: MPLFig = None,
+    phases: bool = True,
+    reactions: bool = True,
+    x1: float = 0,
+    x2: float = 14,
+) -> MPLFig:
     fig, ax = plt.subplots() if plot is None else plot
 
-    drop = np.array([SLOPE*x1, SLOPE*x2])
-    ax.plot([x1, x2], drop + 1.229, 'C0--')
-    ax.plot([x1, x2], drop, 'C1--')
+    drop = np.array([SLOPE * x1, SLOPE * x2])
+    ax.plot([x1, x2], drop + 1.229, "C0--")
+    ax.plot([x1, x2], drop, "C1--")
 
     if phases:
-        ax.text(10,  1.00, 'Oxygen')
-        ax.text( 6,  0.20, 'Water')
-        ax.text( 2, -0.55, 'Hydrogen')
+        ax.text(10, 1.00, "Oxygen")
+        ax.text(6, 0.20, "Water")
+        ax.text(2, -0.55, "Hydrogen")
 
     if reactions:
         # Oxygen evolution
-        ax.text(0.1, 1, 'O$_2$ + 4H$^+$ + 4e$^-$ $\\rightleftharpoons$ 2H$_2$O', rotation=ROTATION)
-        ax.text(8.7, 1 + SLOPE*8.6, 'O$_2$ + 2H$_2$O + 4e$^-$ $\\rightleftharpoons$ 4OH$^-$', rotation=ROTATION)
+        ax.text(
+            0.1,
+            1,
+            "O$_2$ + 4H$^+$ + 4e$^-$ $\\rightleftharpoons$ 2H$_2$O",
+            rotation=ROTATION,
+        )
+        ax.text(
+            8.7,
+            1 + SLOPE * 8.6,
+            "O$_2$ + 2H$_2$O + 4e$^-$ $\\rightleftharpoons$ 4OH$^-$",
+            rotation=ROTATION,
+        )
 
         # Hydrogen evolution
-        ax.text(0.1, -0.15, '2H$^+$ + 2e$^-$ $\\rightleftharpoons$ H$_2$', rotation=ROTATION)
-        ax.text(8.7, -0.25 + SLOPE*8.6, '2H$_2$O + 2e$^-$ $\\rightleftharpoons$ H$_2$ + 2OH$^-$', rotation=ROTATION)
+        ax.text(
+            0.1, -0.15, "2H$^+$ + 2e$^-$ $\\rightleftharpoons$ H$_2$", rotation=ROTATION
+        )
+        ax.text(
+            8.7,
+            -0.25 + SLOPE * 8.6,
+            "2H$_2$O + 2e$^-$ $\\rightleftharpoons$ H$_2$ + 2OH$^-$",
+            rotation=ROTATION,
+        )
 
     return fig, ax
 
 
-def Al(plot=None, phases=True, reactions=True, x1=0, x2=14):
+def Al(
+    plot: MPLFig = None,
+    phases: bool = True,
+    reactions: bool = True,
+    x1: float = 0,
+    x2: float = 14,
+) -> MPLFig:
     fig, ax = plt.subplots() if plot is None else plot
 
-    ax.plot([4, 4], [-1.6, 100], 'C3')
-    ax.plot([8.5, 8.5], [-1.6 + SLOPE*4.5, 100], 'C5')
-    ax.plot([x1, 4], [-1.6, -1.6], 'C2')
-    ax.plot([4, x2], [-1.6, -1.6 + SLOPE*(x2 - 4)], 'C4')
+    ax.plot([4, 4], [-1.6, 100], "C3")
+    ax.plot([8.5, 8.5], [-1.6 + SLOPE * 4.5, 100], "C5")
+    ax.plot([x1, 4], [-1.6, -1.6], "C2")
+    ax.plot([4, x2], [-1.6, -1.6 + SLOPE * (x2 - 4)], "C4")
 
     if phases:
-        ax.text(1.5, 0.4, 'Al$^{3+}$')
-        ax.text(5.3, 0.4 + SLOPE*3.8, 'Al$_2$O$_3$·H$_2$O')
-        ax.text(10.5,  0.4 + SLOPE*9, 'AlO$_2^-$')
-        ax.text(3.5, -2.1, 'Al')
+        ax.text(1.5, 0.4, "Al$^{3+}$")
+        ax.text(5.3, 0.4 + SLOPE * 3.8, "Al$_2$O$_3$·H$_2$O")
+        ax.text(10.5, 0.4 + SLOPE * 9, "AlO$_2^-$")
+        ax.text(3.5, -2.1, "Al")
 
     if reactions:
         pass
@@ -52,15 +87,21 @@ def Al(plot=None, phases=True, reactions=True, x1=0, x2=14):
     return fig, ax
 
 
-def Ir(plot=None, phases=True, reactions=True, x1=0, x2=14):
+def Ir(
+    plot: MPLFig = None,
+    phases: bool = True,
+    reactions: bool = True,
+    x1: float = 0,
+    x2: float = 14,
+) -> MPLFig:
     fig, ax = plt.subplots() if plot is None else plot
 
-    drop = np.array([SLOPE*x1, SLOPE*x2])
-    ax.plot([x1, x2], drop + 0.95, 'C6')
+    drop = np.array([SLOPE * x1, SLOPE * x2])
+    ax.plot([x1, x2], drop + 0.95, "C6")
 
     if phases:
-        ax.text(7.6,  0.55, 'IrO$_2$')
-        ax.text(5.7,  0.15, 'Ir')
+        ax.text(7.6, 0.55, "IrO$_2$")
+        ax.text(5.7, 0.15, "Ir")
 
     if reactions:
         pass
@@ -68,24 +109,30 @@ def Ir(plot=None, phases=True, reactions=True, x1=0, x2=14):
     return fig, ax
 
 
-def Ru(plot=None, phases=True, reactions=True, x1=0, x2=14):
+def Ru(
+    plot: MPLFig = None,
+    phases: bool = True,
+    reactions: bool = True,
+    x1: float = 0,
+    x2: float = 14,
+) -> MPLFig:
     fig, ax = plt.subplots() if plot is None else plot
 
-    drop = np.array([SLOPE*x1, SLOPE*x2])
-    ax.plot([x1, x2], drop + 0.78, 'C2')
-    ax.plot([x1, x2], drop + 0.95, 'C3')
-    ax.plot([x1, 11.2], [1.00, 1.00], 'C4')
-    ax.plot([11.2, 11.2], [1.0, 1.5], 'C5')
-    ax.plot([11.2, 14], [1.0, 1.0 + SLOPE*(x2 - 11.2)], 'C4')
+    drop = np.array([SLOPE * x1, SLOPE * x2])
+    ax.plot([x1, x2], drop + 0.78, "C2")
+    ax.plot([x1, x2], drop + 0.95, "C3")
+    ax.plot([x1, 11.2], [1.00, 1.00], "C4")
+    ax.plot([11.2, 11.2], [1.0, 1.5], "C5")
+    ax.plot([11.2, 14], [1.0, 1.0 + SLOPE * (x2 - 11.2)], "C4")
 
     if phases:
-        ax.text(5.7,  0.05, 'Ru')
-        ax.text(6.0,  0.66, 'RuO$_2\cdot2$H$_2$O', rotation=ROTATION)
-        ax.text(5.9,  0.47, 'Ru(OH$_3$)', rotation=ROTATION)
-        ax.text(5.7,  1.25, 'RuO$_4$')
-        ax.text(10.0, 0.85, 'RuO${_4}^{-}$', rotation=-7)
-        ax.text(12.5, 0.60, 'RuO${_4}^{-2}$', rotation=ROTATION)
-        ax.text(12.0, 1.25, 'HRuO${_5}^{-}$')
+        ax.text(5.7, 0.05, "Ru")
+        ax.text(6.0, 0.66, r"RuO$_2\cdot2$H$_2$O", rotation=ROTATION)
+        ax.text(5.9, 0.47, "Ru(OH$_3$)", rotation=ROTATION)
+        ax.text(5.7, 1.25, "RuO$_4$")
+        ax.text(10.0, 0.85, "RuO${_4}^{-}$", rotation=-7)
+        ax.text(12.5, 0.60, "RuO${_4}^{-2}$", rotation=ROTATION)
+        ax.text(12.0, 1.25, "HRuO${_5}^{-}$")
 
     if reactions:
         pass
@@ -93,62 +140,80 @@ def Ru(plot=None, phases=True, reactions=True, x1=0, x2=14):
     return fig, ax
 
 
-def pic(plot=None, name='', x1=0, x2=14, y1=-1, y2=1.5):
+def pic(
+    plot: MPLFig = None,
+    name: str = "",
+    x1: float = 0,
+    x2: float = 14,
+    y1: float = -1,
+    y2: float = 1.5,
+) -> MPLFig:
     fig, ax = plt.subplots() if plot is None else plot
 
     ax.set_xlim([x1, x2])
     ax.set_ylim([y1, y2])
 
-    make_ticks = lambda start, end, tw: np.arange(int(start/tw)*tw, int(end/tw + 1)*tw, tw)
+    make_ticks = lambda start, end, tw: np.arange(
+        int(start / tw) * tw, int(end / tw + 1) * tw, tw
+    )
 
-    #ax.set_yticks(make_ticks(y1, y2, 0.5))
+    # ax.set_yticks(make_ticks(y1, y2, 0.5))
 
     if name:
         ax.set_title(name)
-    ax.set_ylabel('Potential E(V) vs SHE')
-    ax.set_xlabel('pH')
+    ax.set_ylabel("Potential E(V) vs SHE")
+    ax.set_xlabel("pH")
+
+    return fig, ax
 
 
-def water_pic(plot=None):
+def water_pic(plot: MPLFig = None) -> MPLFig:
     plot = plt.subplots() if plot is None else plot
 
-    pic(plot, 'Water Pourbaix Diagram')
+    pic(plot, "Water Pourbaix Diagram")
     water(plot)
-    plot[0].savefig('water.pdf')
+    plot[0].savefig("water.pdf")
     plot[0].show()
 
+    return plot
 
-def Al_pic(plot=None):
+
+def Al_pic(plot: MPLFig = None) -> MPLFig:
     plot = plt.subplots() if plot is None else plot
 
-    pic(plot, 'Al Pourbaix Diagram', y1=-2.5, y2=1.5)
+    pic(plot, "Al Pourbaix Diagram", y1=-2.5, y2=1.5)
     water(plot, phases=False)
     Al(plot)
-    plot[0].savefig('Al_water.pdf')
+    plot[0].savefig("Al_water.pdf")
     plot[0].show()
 
+    return plot
 
-def Ir_pic(plot=None):
+
+def Ir_pic(plot: MPLFig = None) -> MPLFig:
     plot = plt.subplots() if plot is None else plot
 
-    pic(plot, 'Ir Pourbaix Diagram')
+    pic(plot, "Ir Pourbaix Diagram")
     water(plot, phases=False)
     Ir(plot)
-    plot[0].savefig('Ir_water.pdf')
+    plot[0].savefig("Ir_water.pdf")
     plot[0].show()
 
+    return plot
 
-def Ru_pic(plot=None):
+
+def Ru_pic(plot: MPLFig = None) -> MPLFig:
     plot = plt.subplots() if plot is None else plot
 
-    pic(plot, 'Ru Pourbaix Diagram')
+    pic(plot, "Ru Pourbaix Diagram")
     water(plot, phases=False, reactions=False)
     Ru(plot)
-    plot[0].savefig('Ru_water.pdf')
-    plot[0].show()
+    plot[0].savefig("Ru_water.pdf")
+
+    return plot
 
 
-water_pic()
-Al_pic()
-Ir_pic()
-Ru_pic()
+water_pic()[0].show()
+Al_pic()[0].show()
+Ir_pic()[0].show()
+Ru_pic()[0].show()
